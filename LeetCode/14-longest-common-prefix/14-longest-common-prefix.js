71b0fb11e9742d2ad4e19e9461e49dda59f5377a
SUKTAE KIM <91834666+USKTEA@users.noreply.github.com>
2022-04-15 03:40:54 +0900

    Time: 69 ms (80.95%), Space: 43.6 MB (29.61%) - LeetHub

--- LeetCode/14-longest-common-prefix/14-longest-common-prefix.js
+++ LeetCode/14-longest-common-prefix/14-longest-common-prefix.js
@@ -4,21 +4,19 @@
  */
 var longestCommonPrefix = function(strs) {
     let a = strs[0].split('');
-    let b = [];
     if(strs.length === 1) {
         return strs.join();
     }
     for(let i = 1; i < strs.length; i++) {
         for(let j = 0; j < a.length; j++) {
             if(a[j] === strs[i][j]) {
-                b.push(a[j])
+                continue;
             } if(a[j] !== strs[i][j]) {
                 a.splice(j)
             }
         }
     }
-    console.log(b)
-    console.log(a)
+
     let c = "";
     for(let i = 0; i < a.length ; i++) {
         c = c + a[i]
