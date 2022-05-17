d6a06657a7fbae29653271410ee1b46948c269ce
SUKTAE KIM <91834666+USKTEA@users.noreply.github.com>
2022-05-17 13:32:10 +0900

    Time: 99 ms (26.11%), Space: 41.9 MB (82.68%) - LeetHub

---
+++ LeetCode/28-implement-strstr/28-implement-strstr.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  const compare = haystack.split('');
+  const test = needle.split('');
+  const total = compare.concat(test);
+  let left = 0;
+  let right = haystack.length;
+  let index = -1;
+  let temp = 0;
+
+  if (haystack.length < needle.length) {
+    return index;
+  }
+
+  while (right < total.length && left < haystack.length) {
+    if (total[left] === total[right]) {
+      if (index === -1) {
+        index = left;
+        temp = left;
+      }
+
+      left++;
+      right++;
+    } else {
+      temp = temp + 1;
+      right = haystack.length;
+      left = temp;
+      index = -1;
+    }
+  }
+
+    if (right === total.length) {
+    return index;
+  }
+
+  return -1;
+};
