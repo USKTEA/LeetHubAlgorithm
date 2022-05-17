886766a5cc8f8688fb70571bb9445f7d219f64bb
SUKTAE KIM <91834666+USKTEA@users.noreply.github.com>
2022-05-18 01:03:47 +0900

    Time: 105 ms (21.25%), Space: 45.4 MB (14.76%) - LeetHub

---
+++ LeetCode/704-binary-search/704-binary-search.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target, l = 0, r = nums.length - 1) {
+    const left = l;
+    const right = r;
+    const half = Math.floor((left + right) / 2);
+    
+
+    if (nums[half] === target) {
+        return half;
+    }
+    
+        if (left === right) {
+        return -1;
+    }
+
+
+    if (nums[half] > target) {
+        return search(nums, target, left, half - 1);
+    }
+    
+    if (nums[half] < target) {
+        return search(nums, target, half + 1, right);
+    }
+    
+    return -1;
+};
