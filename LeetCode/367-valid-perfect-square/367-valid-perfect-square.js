3bd36c399d671ef3df55bc6bb5886755e5b4b749
SUKTAE KIM <91834666+USKTEA@users.noreply.github.com>
2022-05-20 01:34:32 +0900

    Time: 81 ms (48.47%), Space: 42.3 MB (19.22%) - LeetHub

---
+++ LeetCode/367-valid-perfect-square/367-valid-perfect-square.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+  let half = Math.floor(num / 2);
+  let prevHalf = 0;
+  
+  if (num === 1 || half * half === num) {
+    return true;
+  }
+    
+  while (half * half > num) {
+    half = Math.floor(half / 2);
+
+    if (half * half === num) {
+      return true;
+    }
+
+    prevHalf = half * 2;
+  }
+
+  while (half < prevHalf) {
+    if (half * half === num) {
+      return true;
+    }
+
+    half++;
+  }
+
+  return false;
+};
