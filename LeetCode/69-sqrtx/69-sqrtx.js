5b503eaf8eba5543f957d1cebdbdc92049f315fd
SUKTAE KIM <91834666+USKTEA@users.noreply.github.com>
2022-05-21 20:15:57 +0900

    Time: 114 ms (41.00%), Space: 44.4 MB (9.79%) - LeetHub

--- LeetCode/69-sqrtx/69-sqrtx.js
+++ LeetCode/69-sqrtx/69-sqrtx.js
@@ -5,26 +5,25 @@
 var mySqrt = function(x) {
     let min = 0;
     let max = x;
-    let mid = Math.trunc(min + max / 2);
-    let temp = mid;
     
-    if (x === 0) {
-        return 0;
+    
+    while (min <= max) {
+        let mid = Math.floor((min + max) / 2);
+        
+        if (mid * mid > x) {
+            max = mid - 1;
+        }
+        
+        if (mid * mid < x) {
+            min = mid + 1;
+        }
+        
+        if (mid * mid === x) {
+            max = mid;
+            
+            break;
+        }
     }
     
-    if (x <= 2) {
-        return 1;
-    }
-    
-    while (mid * mid > x) {
-        mid = Math.trunc((min + mid) / 2);
-        max = mid;
-    }
-    
-    while (mid * mid <= x) {
-        temp = mid;
-        mid = mid + 1;
-    }
-    
-    return temp;
+    return max;
 };
