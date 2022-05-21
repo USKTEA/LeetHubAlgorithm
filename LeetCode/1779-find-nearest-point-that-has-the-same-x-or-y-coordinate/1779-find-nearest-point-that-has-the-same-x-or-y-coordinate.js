04d133408ec00a393011067582f6fb9d6d3a7fb6
SUKTAE KIM <91834666+USKTEA@users.noreply.github.com>
2022-05-21 13:37:35 +0900

    Time: 124 ms (56.19%), Space: 49.8 MB (98.83%) - LeetHub

--- LeetCode/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
+++ LeetCode/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
@@ -6,36 +6,29 @@
  */
 var nearestValidPoint = function(x, y, points) {
   const myLocation = [x, y];
-  let distance = [];
   let index = 0;
-  let result;
+  let distance = -1;
+  let distanceIndex = -1;
 
   for (const value of points) {
     if (value[0] === myLocation[0] || value[1] === myLocation[1]) {
       const manhattanDisntance =
         Math.abs(myLocation[0] - value[0]) + Math.abs(myLocation[1] - value[1]);
-      distance.push({ [index]: manhattanDisntance });
+      if (distance === -1) {
+        distance = manhattanDisntance;
+        distanceIndex = index;
+      }
+
+      if (distance > manhattanDisntance) {
+        distance = manhattanDisntance;
+        distanceIndex = index;
+      }
+
       index++;
     } else {
       index++;
     }
   }
 
-  if (distance.length === 0) {
-    return -1;
-  }
-
-  for (const value of distance) {
-    for (const [distanceIndex, distance] of Object.entries(value)) {
-      if (!result) {
-        result = { distanceIndex, distance };
-      }
-
-      if (result.distance > distance) {
-        result = { distanceIndex, distance };
-      }
-    }
-  }
-
-  return Number(result.distanceIndex);
+  return distanceIndex;
 };
