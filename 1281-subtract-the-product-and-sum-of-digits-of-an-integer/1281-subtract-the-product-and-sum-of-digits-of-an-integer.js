394c7bc7d4f97759b51d8ccf382ba9a0befe639b
SUKTAE KIM <91834666+USKTEA@users.noreply.github.com>
2022-08-03 23:24:15 +0900

    Time: 95 ms (37.41%), Space: 42 MB (66.38%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var subtractProductAndSum = function(n) {
-    const inputs = [...n.toString()];
+    const inputs = Array.from(String(n), Number);
     const answer = inputs.reduce((total, current, index) => {
         total[0] = total[0] * Number(current);
         total[1] = total[1] + Number(current);
