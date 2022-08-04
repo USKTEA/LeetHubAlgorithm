66d9fd63bdf25b6dd665dcf2d035272a13a0aff6
SUKTAE KIM <91834666+USKTEA@users.noreply.github.com>
2022-08-05 00:37:26 +0900

    Time: 185 ms (13.48%), Space: 50.2 MB (88.33%) - LeetHub

---
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number[][]} points
+ * @return {number}
+ */
+var nearestValidPoint = function(x, y, points) {
+    let min = Infinity
+    let idx = -1
+    points.forEach(([a,b], i)=>{
+        if(a===x || b===y){
+            const dist = Math.abs(x-a) + Math.abs(y-b)
+            if(dist<min){
+                idx = i
+                min = dist
+            }
+        }
+    })
+    return idx
+};
